118dfe0e86d2b532b627fae101a251092e2e283b
azmain-siam <siamhossain2700@gmail.com>
2024-06-12 02:27:53 +0600

    Finised content management page

--- index.js
+++ index.js
@@ -211,6 +211,22 @@ async function run() {
       res.send(result);
     });
 
+    // Get single blog from blogs collection
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // delete single blog from blogs collection
+    app.delete("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Update status of blog
     app.patch("/blog/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;

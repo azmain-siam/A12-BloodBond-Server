c5d2474795294b811ff33d7656f78015d327ef70
azmain-siam <siamhossain2700@gmail.com>
2024-06-12 18:56:08 +0600

    Final Commit

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://blood-bond-f0feb.web.app",
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
